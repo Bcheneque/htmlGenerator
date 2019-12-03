acee99163643f6a1c9a843df54934592c8253d5b
bradxy cheneque <bachacheneque@gmail.com>
2019-12-03 10:03:00 -0800

    express server set up

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const htpp = require("http").Server(app);
+const port = 3000;
+http.listen(port);
+console.log("Express");
+
+//$.post("http://localhost:3000/submitComment", data, (dataFromServer) => {
+  //                   console.log("The server has responded.");
